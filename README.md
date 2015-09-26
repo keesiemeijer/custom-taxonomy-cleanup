c7aeeb4e3de19ca5dcb569afd69fca08d7b7e98f
keesiemeijer <keesie.meijer@gmail.com>
2015-09-26 13:21:01 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# custom-taxonomy-cleanup
-A WordPress plugin to detect and delete terms from custom taxonomies that are no longer in use
+# Custom Taxonomy CleanUp
+
+version:         1.0  
+Tested up to WP: 4.3  
+
+Custom taxonomy terms are left in the database if a taxonomy is no longer registered (in use). 
+
+Plugins and themes can (without you knowing) use custom taxomies as a way to store data. These terms stay in the database forever if they're not cleaned up by the plugin/theme itself upon deletion.
+
+This plugin provides an easy way to detect and remove terms from taxonomies that are no longer in use.
+
+**Note**: The proper WordPress delete function `wp_delete_term()` is used instead of running a direct MySQL query to delete the terms. 
+
+It's recommended you make a database backup before deleting terms.
---
+++ custom-taxonomy-cleanup.js
@@ -0,0 +1,40 @@
+( function( $ ) {
+
+	$( document ).ready( function() {
+
+		if ( typeof ctc_plugin === 'undefined' ) {
+			return;
+		}
+
+		if ( !$( ctc_plugin ).length ) {
+			return;
+		}
+
+		if ( ctc_plugin.remove_storage ) {
+			localStorage.removeItem( "ctc_run_delete_dialog" );
+		}
+
+
+
+		$( "input[name='custom_taxonomy_cleanup']" ).on( "click", function( e ) {
+
+			if ( ( 'localStorage' in window ) && ( window[ 'localStorage' ] !== null ) ) {
+
+				var taxonomy = $( "#ctc_taxonomy" ).val();
+				var storage = localStorage.getItem( "ctc_run_delete_dialog" );
+
+				if ( taxonomy.length && ( taxonomy !== storage ) ) {
+
+					// Remove confirm dialog after first delete terms dialog.
+					localStorage.setItem( "ctc_run_delete_dialog", taxonomy );
+
+					var confirm_msg = ctc_plugin.confirm.replace( /%s/g, taxonomy );
+
+					return confirm( confirm_msg );
+				}
+			}
+
+		} );
+	} );
+
+} )( jQuery );
